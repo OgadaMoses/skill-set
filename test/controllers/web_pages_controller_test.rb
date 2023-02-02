51e864123c11087c7a392e6a612032d135a4f087
Ogada Moses <ogadamoses59@gmail.com>
2023-02-02 16:58:45 +0300

    Add tittle page

--- test/controllers/web_pages_controller_test.rb
+++ test/controllers/web_pages_controller_test.rb
@@ -4,51 +4,61 @@ class WebPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get web_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Skillset"
   end
 
   test "should get about" do
     get web_pages_about_url
     assert_response :success
+    assert_select "title", "About | Skillset"
   end
 
   test "should get courses" do
     get web_pages_courses_url
     assert_response :success
+    assert_select "title", "Courses | Skillset"
   end
 
   test "should get contact" do
     get web_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Skillset"
   end
 
   test "should get register" do
     get web_pages_register_url
     assert_response :success
+    assert_select "title", "Register | SKillset"
   end
 
   test "should get login" do
     get web_pages_login_url
     assert_response :success
+    assert_select "title", "Login | SKillset"
   end
 
   test "should get help" do
     get web_pages_help_url
     assert_response :success
+    assert_select "title", "Help | SKillset"
   end
 
   test "should get blog" do
     get web_pages_blog_url 
     assert_response :success
+    assert_select "title", "Blog | SKillset"
   end 
 
   test "should get library" do 
     get web_pages_library_url 
     assert_response :success
+    assert_select "title", "Library | SKillset"
   end
 
   test "should get alumni" do
     get web_pages_alumni_url 
     assert_response :success
+    assert_select "title", "Alumni | SKillset"
   end
 
 end
