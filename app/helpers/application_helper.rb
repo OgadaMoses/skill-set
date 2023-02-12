b8822090afcbe270a9b6707f643f61d3d36a56ef
Ogada Moses <ogadamoses59@gmail.com>
2023-02-12 15:36:20 +0300

    add application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns the full page title on per page basis #
+ def full_title(page_title='')
+    base_title = "Skillset"
+    if page_title.empty?
+        base_title
+    else
+        "#{page_title} | #{base_title}"
+    end
+ end    
 end
