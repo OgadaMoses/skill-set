de94824216741cb7f85c43d92f5175b11aec4960
Ogada Moses <ogadamoses59@gmail.com>
2023-02-25 10:31:14 +0300

    Add alumni model validations and tests

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,2 +1,6 @@
 class Alumni < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :year, presence: true
+    validates :course, presence: true
 end
--- test/models/alumni_test.rb
+++ test/models/alumni_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class AlumniTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+  @alumni = Alumni.new(name: "Example Alumni", email: "alumni@example.com", year: "2023", course: "ICT")
+  end
+
+  test "should be valid" do
+    assert @alumni.valid?
+  end
+
+  test "name should be present" do 
+    @alumni.name = "  "
+    assert_not @alumni.valid?
+  end
+  
+  test "email should be present" do 
+    @alumni.email = " "
+    assert_not @alumni.valid?
+  end
+
+  test "study year be present" do 
+    @alumni.year = " "
+    assert_not @alumni.valid?
+  end
+
+  test "course be present" do 
+    @alumni.course = " "
+    assert_not @alumni.valid? 
+  end
 end
